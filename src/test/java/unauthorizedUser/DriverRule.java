3b79517a2ad1d4fe306ad7fadce68d37084a187f
Marina Riabogina <marina.ryabogina@yandex.ru>
2023-11-05 11:34:37 +0700

    добавлена конфигурации для браузеров Google Chrome и Яндекс.Браузер

--- src/test/java/unauthorizedUser/DriverRule.java
+++ src/test/java/unauthorizedUser/DriverRule.java
@@ -4,6 +4,8 @@ import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import stellarBurger.EnvConfig;
 
 import java.io.File;
 import java.time.Duration;
@@ -14,11 +16,32 @@ public class DriverRule extends ExternalResource {
     @Override
     protected void before() throws Throwable {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
-        ChromeDriverService service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File("/C:/WebDriver/bin/chromedriver.exe"))
-                .build();
-        driver = new ChromeDriver(service);
+        if ("yandex".equals(System.getProperty("browser")))
+            setUpYandex();
+        else
+            setUpChrome();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    private void setUpChrome() {
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(EnvConfig.CHROME_DRIVER))
+                .build();
+
+        ChromeOptions options = new ChromeOptions()
+                .setBinary(EnvConfig.CHROME_BINARY);
+
+        driver = new ChromeDriver(service, options);
+    }
+
+    public void setUpYandex() {
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(EnvConfig.YANDEX_DRIVER))
+                .build();
+        ChromeOptions options = new ChromeOptions()
+                .setBinary(EnvConfig.YANDEX_BINARY);
+
+        driver = new ChromeDriver(service, options);
     }
 
     @Override
