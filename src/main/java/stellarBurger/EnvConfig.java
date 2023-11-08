0616feb5676829a9cb4405ac35b5a9896c4fb0fd
Marina Riabogina <marina.ryabogina@yandex.ru>
2023-11-08 14:29:55 +0700

    удалены константы для браузера Chrome, изменена констатнта для драйвера Яндекс Браузера

--- src/main/java/stellarBurger/EnvConfig.java
+++ src/main/java/stellarBurger/EnvConfig.java
@@ -1,8 +1,7 @@
 package stellarBurger;
 
 public class EnvConfig {
-    public static final String CHROME_DRIVER = System.getProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-    public static final String CHROME_BINARY = System.getProperty("webdriver.chrome.binary", "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-    public static final String YANDEX_DRIVER = System.getProperty("webdriver.yandex.driver", "C:\\WebDriver\\bin\\yandexdriver.exe");
-    public static final String YANDEX_BINARY = System.getProperty("webdriver.yandex.binary", "C:\\Program Files\\Yandex\\YandexBrowser\\Application\\browser.exe");
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+    public static final String YANDEX_DRIVER = System.getProperty("webdriver.yandex.driver", "./src/main/resources/yandexdriver.exe");
+    public static final String YANDEX_BINARY = System.getProperty("webdriver.yandex.binary", "C:/Program Files/Yandex/YandexBrowser/Application/browser.exe");
 }
